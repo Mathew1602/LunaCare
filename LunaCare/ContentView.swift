b0998293d4c39a5fa7ccc6e27ad2e16d45342f31
Mathewboyd <MathewBoyd>
2025-09-11 10:24:03 -0400

    Initial Commit

---
+++ LunaCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LunaCare
+//
+//  Created by Mathew Boyd on 2025-09-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world this is Luna Care")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LunaCare/LunaCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaCareApp.swift
+//  LunaCare
+//
+//  Created by Mathew Boyd on 2025-09-11.
+//
+
+import SwiftUI
+
+@main
+struct LunaCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
